74fbacae3ebdd79efe27d7b74c72790cbeadef3f
jun.li <jun.li@generaltest.com>
2018-09-15 18:26:59 +0800

    The First version 2018-09-15

---
+++ Sub6g-GTS/SaveData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sub6g_GTS
+{
+    class SaveData
+    {
+        public void Save ()
+        {
+            string a1 = a.text;
+
+        }
+        public void Display()
+        {
+
+        }
+    }
+}
